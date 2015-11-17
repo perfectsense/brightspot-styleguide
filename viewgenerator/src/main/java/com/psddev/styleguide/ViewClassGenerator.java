45177c58b46212a3ae8b3df4a004f6ce9e999797
Dan Slaughter <dan@perfectsensedigital.com>
2015-11-17 15:11:01 -0500

    Removes unused imports.

--- viewgenerator/src/main/java/com/psddev/styleguide/ViewClassGenerator.java
+++ viewgenerator/src/main/java/com/psddev/styleguide/ViewClassGenerator.java
@@ -6,10 +6,8 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 

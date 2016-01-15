99e4226f0cd5cefad56ad941f2f6483bf34aaa8c
Dan Slaughter <dan@perfectsensedigital.com>
2016-01-15 13:07:34 -0500

    Makes variable private again.

--- viewgenerator/src/main/java/com/psddev/styleguide/JsonDataFiles.java
+++ viewgenerator/src/main/java/com/psddev/styleguide/JsonDataFiles.java
@@ -20,7 +20,7 @@ import com.psddev.dari.util.StringUtils;
 
 class JsonDataFiles {
 
-    public Map<String, JsonDataFile> dataFilesByFile;
+    private Map<String, JsonDataFile> dataFilesByFile;
 
     private Map<String, List<JsonDataFile>> dataFilesByTemplate;
 

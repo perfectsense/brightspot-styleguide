e6f2e7d0685fcffdb5118d991243e281c17549d5
Bryan Morgan <bryanmorgan@gmail.com>
2016-05-31 11:09:57 -0700

    Update exceptions to include `_view` since it is a valid top-level key

--- viewgenerator/src/main/java/com/psddev/styleguide/DataUrlTemplateOverrideException.java
+++ viewgenerator/src/main/java/com/psddev/styleguide/DataUrlTemplateOverrideException.java
@@ -3,6 +3,6 @@ package com.psddev.styleguide;
 public class DataUrlTemplateOverrideException extends RuntimeException {
 
     public DataUrlTemplateOverrideException(JsonDataFile jsonDataFile, String dataUrl) {
-        super("Error in [" + jsonDataFile.getFileName() + "] for _dataUrl [" + dataUrl + "]. Can't override _template field!");
+        super("Error in [" + jsonDataFile.getFileName() + "] for _dataUrl [" + dataUrl + "]. Can't override _template or _view field!");
     }
 }

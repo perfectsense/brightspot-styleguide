7e3a5b7d295182cb77d8e8ad6dd0c678460d76ed
Bryan Morgan <bryanmorgan@gmail.com>
2016-05-31 11:02:33 -0700

    Add template format support to allow both Handlebars and JSON templates

--- viewgenerator/src/main/java/com/psddev/styleguide/JsonTemplateObject.java
+++ viewgenerator/src/main/java/com/psddev/styleguide/JsonTemplateObject.java
@@ -1,11 +1,19 @@
 package com.psddev.styleguide;
 
+import static com.psddev.styleguide.JsonTemplateObject.TemplateFormat.Handlebars;
+import static com.psddev.styleguide.JsonTemplateObject.TemplateFormat.Json;
+
 import java.util.Collections;
 import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Set;
 
+import com.psddev.dari.util.ObjectUtils;
+
 class JsonTemplateObject extends JsonObject {
+
+    public static final String TEMPLATE_KEY = "_template";
+    public static final String JSON_VIEW_KEY = "_view";
 
     private String template;
 
@@ -13,12 +21,23 @@ class JsonTemplateObject extends JsonObject {
 
     private String fieldNotes;
     private String templateNotes;
+    private TemplateFormat templateFormat;
 
-    public JsonTemplateObject(String template, Map<String, JsonObject> fields, String fieldNotes, String templateNotes) {
+    public enum TemplateFormat {
+        Handlebars,
+        Json
+    }
+
+    public JsonTemplateObject(String template,
+                              Map<String, JsonObject> fields,
+                              String fieldNotes,
+                              String templateNotes,
+                              TemplateFormat templateFormat) {
         this.template = template;
         this.fields = fields != null ? fields : Collections.emptyMap();
         this.fieldNotes = fieldNotes;
         this.templateNotes = templateNotes;
+        this.templateFormat = templateFormat;
     }
 
     public String getTemplateName() {
@@ -40,6 +59,24 @@ class JsonTemplateObject extends JsonObject {
 
     public String getTemplateNotes() {
         return templateNotes;
+    }
+
+    public TemplateFormat getTemplateFormat() {
+        return templateFormat;
+    }
+
+    public static String getTemplateName(Map<?, ?> map) {
+        return (String) ObjectUtils.firstNonBlank(map.get(TEMPLATE_KEY), map.get(JSON_VIEW_KEY));
+    }
+
+    public static TemplateFormat getTemplateFormat(Map<?, ?> map) {
+        if (!ObjectUtils.isBlank(map.get(TEMPLATE_KEY))) {
+            return Handlebars;
+        } else if (!ObjectUtils.isBlank(map.get(JSON_VIEW_KEY))) {
+            return Json;
+        }
+
+        return null;
     }
 
     @Override

7d121ffa7d7ef8061178872b4538ecb18ffba508
John Pencola <paulpencola@gmail.com>
2016-10-19 15:42:20 -0400

    Add sourceMap support

--- tasks/compile.js
+++ tasks/compile.js
@@ -1,15 +1,15 @@
 const gulp = require('gulp');
 const less = require('gulp-less');
 const uglify = require('gulp-uglify');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
+const sourceMaps = require('gulp-sourcemaps');
 const SystemjsBuilder = require('gulp-systemjs-builder');
 
 module.exports.styles = function (path) {
-    if (!path) {
-        path = [ '_build' ];
-    }
+    path = path || [ '_build' ]
+
     return less({
-      paths: path
+        paths: path
     })
 }
 
@@ -18,6 +18,13 @@ module.exports.scripts = function (config) {
         minify: false,
         mangle: false
     }
+
+    /*,
+    externals: ['jquery'],
+    globalDeps: {
+        'jquery.js': '$'
+    }
+    */
 
     var systemConfig = {
         defaultJSExtensions: true
@@ -29,8 +36,10 @@ module.exports.scripts = function (config) {
     // SystemJS Builder API: https://github.com/systemjs/builder
     return builder
             .buildStatic('All.js', builderConfig)
+            .pipe(sourceMaps.init())
             .pipe(gulp.dest('_dist'))
             .pipe(uglify())
             .pipe(rename({extname: '.min.js'}))
+            .pipe(sourceMaps.write('.'))
             .pipe(gulp.dest('_dist'))
 }

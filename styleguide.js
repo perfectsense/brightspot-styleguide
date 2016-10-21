54da57dbf8c1e140e595884606aee12d0928c906
John Pencola <paulpencola@gmail.com>
2016-10-21 11:57:17 -0400

    Convert styleguide to return an anon object

--- styleguide.js
+++ styleguide.js
@@ -1,22 +1,7 @@
-const paths = require('./gulp/paths.js')
-
-var Styleguide = function (config) {
-    this.config = config
+let Styleguide = {
+    distPath() {
+        return '_dist'
+    }
 }
-
-Styleguide.prototype.distRoot = function () {
-    return paths.distRoot
-};
-
-Styleguide.prototype.buildRoot = function () {
-    return paths.buildRoot
-};
-
-Styleguide.prototype.clean = require('./gulp/clean.js').clean;
-Styleguide.prototype.copySrc = require('./gulp/copy.js').copySrc;
-Styleguide.prototype.bowerInstall = require('./gulp/bower.js').bowerInstall;
-Styleguide.prototype.copyBower = require('./gulp/bower.js').copyBower;
-Styleguide.prototype.compileScripts = require('./gulp/compile.js').scripts;
-Styleguide.prototype.compileStyles = require('./gulp/compile.js').styles;
 
 module.exports = Styleguide;

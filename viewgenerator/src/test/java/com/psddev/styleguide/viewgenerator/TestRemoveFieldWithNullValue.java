e562d4ea026358fda034bcd9189707fd2b0ec7d5
Dan Slaughter <dan@perfectsensedigital.com>
2016-11-02 15:10:42 -0400

    Renames test method to be consistent with actual test.

--- viewgenerator/src/test/java/com/psddev/styleguide/viewgenerator/TestRemoveFieldWithNullValue.java
+++ viewgenerator/src/test/java/com/psddev/styleguide/viewgenerator/TestRemoveFieldWithNullValue.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class TestRemoveFieldWithNullValue {
 
     @Test
-    public void testJsonDirectory() throws Exception {
+    public void testNull() throws Exception {
 
         ViewClassGenerator generator = TestUtils.getDefaultGeneratorForClass(getClass());
 

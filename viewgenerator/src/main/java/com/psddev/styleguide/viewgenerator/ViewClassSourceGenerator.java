ec17742e1826f36ac305d39605c597fbeb6b3a6c
Dan Slaughter <dan@perfectsensedigital.com>
2016-11-07 17:51:44 -0500

    Fixes checkstyle violation.

--- viewgenerator/src/main/java/com/psddev/styleguide/viewgenerator/ViewClassSourceGenerator.java
+++ viewgenerator/src/main/java/com/psddev/styleguide/viewgenerator/ViewClassSourceGenerator.java
@@ -3,7 +3,6 @@ package com.psddev.styleguide.viewgenerator;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;

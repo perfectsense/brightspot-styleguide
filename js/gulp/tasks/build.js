0712db6edfc3291cd6553bac04b810f5c9aba5a1
Hyoo Lim <hyoolim@gmail.com>
2016-12-05 22:05:47 -0500

    Changes example files to be statically generated.

--- js/gulp/tasks/build.js
+++ js/gulp/tasks/build.js
@@ -3,14 +3,16 @@ const path = require('path')
 const extensionPattern = '{hbs,json}'
 const file = require('gulp-file')
 const filter = require('gulp-filter')
+const handlebars = require('handlebars')
 const less = require('gulp-less')
 const gutil = require('gulp-util')
 const glob = require('glob')
+const sentenceCase = require('sentence-case')
 const through = require('through2')
 
 module.exports = {
   registerModule: (styleguide) => {
-    let gulp = styleguide._gulp;
+    let gulp = styleguide._gulp
 
     gulp.task('styleguide:build:server:fonts', [ ], () => {
       return gulp.src(path.join(path.dirname(require.resolve('font-awesome/package.json')), 'fonts', '*'))
@@ -30,7 +32,7 @@ module.exports = {
       return gulp.src(path.join(styleguide.path.build(), '**/*.json'))
         .pipe(through.obj((file, encoding, callback) => {
           if (!fs.statSync(file.path).isDirectory() && path.basename(file.path).slice(0, 1) !== '_') {
-            file.contents = new Buffer(require('../../example-file')(styleguide.config, file.path));
+            file.contents = new Buffer(require('../../example-file')(styleguide.config, file.path))
             file.path = gutil.replaceExtension(file.path, '.html')
           }
           callback(null, file)
@@ -38,7 +40,46 @@ module.exports = {
         .pipe(gulp.dest(styleguide.path.build()))
     })
 
-    gulp.task('styleguide:build', [ 'styleguide:build:html', 'styleguide:build:server' ], () => {
+    gulp.task('styleguide:build:index', [ 'styleguide:build:html' ], (done) => {
+      glob('**/*.json', { cwd: styleguide.path.build(), ignore: '**/_*.json' }, (errors, files) => {
+        const groupByName = { }
+
+        files.forEach((file) => {
+          const groupName = path.dirname(file).split('/').map(sentenceCase).join(': ')
+          let group = groupByName[groupName]
+
+          if (!group) {
+            group = groupByName[groupName] = {
+              name: groupName,
+              children: [ ]
+            }
+          }
+
+          group.children.push({
+            name: sentenceCase(path.basename(file, '.json')),
+            url: '/' + gutil.replaceExtension(file, '.html')
+          })
+        })
+
+        const groups = [ ]
+
+        Object.keys(groupByName).sort().forEach((groupName) => {
+          groups.push(groupByName[groupName])
+        });
+
+        const template = handlebars.compile(fs.readFileSync(path.join(__dirname, '..', '..', 'styleguide.hbs'), 'utf8'), {
+          preventIndent: true
+        })
+
+        fs.writeFileSync(path.join(styleguide.path.build(), 'styleguide.html'), template({
+          groups: groups
+        }))
+
+        done()
+      })
+    })
+
+    gulp.task('styleguide:build', [ 'styleguide:build:index', 'styleguide:build:server' ], () => {
     })
   }
 }
--- js/server.js
+++ js/server.js
@@ -1,19 +1,13 @@
-var express = require('express')
-var fs = require('fs')
-var path = require('path')
-var sentenceCase = require('sentence-case')
-var url = require('url')
-var querystring = require('querystring')
+const express = require('express')
+const opn = require('opn')
 
-var label = require('./label')
-var Project = require('./project')
-var logger = require('./logger')
-var renderPage = require('./render-page')
+const Project = require('./project')
+const logger = require('./logger')
 
 module.exports = function (config) {
   logger.welcome()
 
-  let app = express()
+  const app = express()
 
     // Automatically generated placeholder images.
   app.use(require('./placeholder-image')())
@@ -25,148 +19,6 @@ module.exports = function (config) {
   logger.success(` \u{1F539} source: ${config.source}`)
   logger.success(` \u{1F539} build: ${config.build}`)
 
-    // Main display.
-  app.use(function (req, res, next) {
-    var context = { }
-
-    context.project = project
-
-        // Request URL (e.g. /foo/bar) to file path (e.g. \foo\bar in Windows).
-    var requestedUrl = req.path
-
-    context.requestedUrl = requestedUrl
-
-    var requestedPath = context.requestedPath = path.join.apply(path, requestedUrl.split('/'))
-
-        // Styleguide names based on the request URL.
-    var name = req.path.replace(/^\/|\/$/g, '')
-
-    if (name.length > 0) {
-      context.names = name.split('/').map(function (part) {
-        return label(part)
-      })
-    }
-
-    var originalUrlSearch = context.originalUrlSearch = url.parse(req.originalUrl).search || ''
-
-    context.reqPath = req.path
-    context.contentQueryString = querystring.stringify(req.query) || ''
-
-    var examples = context.examples = [ ]
-    var groupNavigation = context.groupNavigation = [ ]
-        // Finds all groups of examples in the styleguide directory.
-
-    project.forEachStyleguideItem(function (group, groupPath) {
-      var oldDirectory = null
-
-      function traverseDirectory (dirPath, groupName) {
-        var name = path.basename(dirPath)
-        var item = {}
-        var group = {}
-
-        if (name.slice(0, 1) !== '_') {
-          var urlPath = dirPath.split(project._targetPath).pop()
-
-          if (path.extname(urlPath) === config.jsonSuffix) {
-                        // item.name = label(name);
-            item = path.parse(urlPath)
-            item.url = path.join(item.dir, item.name) + originalUrlSearch
-            item.name = label(sentenceCase(item.name))
-
-            let formatedLabel = (item.dir.charAt(0) === '/') ? item.dir.substr(1).replace(/\//g, ' / ') : item.dir.replace(/\//g, ' / ')
-            // if formatedLabel is empty set to item.label
-            formatedLabel = formatedLabel || item.name
-            item.dirName = label(formatedLabel, /[^a-zA-Z0-9_/]/g)
-
-            if (oldDirectory !== item.dir) {
-              examples.push(group)
-              group.name = item.dirName
-              groupNavigation = []
-            }
-                        // if the json file is at the root object will not have children.
-                        // push item directly to examples
-            if (groupName) {
-              examples.pop()
-              examples.push(item)
-            } else {
-              groupNavigation.push(item)
-              group.children = groupNavigation
-            }
-
-            oldDirectory = item.dir
-          } else if (fs.statSync(dirPath).isDirectory()) {
-            try {
-                            // if its a directory delete the url so that it doesn't genearate a clickable path.
-              fs.readdirSync(dirPath)
-                                .forEach(child => traverseDirectory(path.join(dirPath, child)))
-                                .filter(e => !!e)
-            } catch (ex) {
-              if (ex.code === 'EACCES') {
-                return null
-              }
-            }
-          } else {
-            return null // Or set item.size = 0 for devices, FIFO and sockets ?
-          }
-          return item
-        }
-      }
-      traverseDirectory(groupPath, group)
-    })
-
-        // Which devices to display?
-    context.resetDisplayDevicesUrl = url.parse(req.originalUrl, true)
-    var availableDevices = context.availableDevices = [ ]
-    var displayDevices = context.displayDevices = [ ]
-
-    config.devices.forEach(function (device, i) {
-      var key = 'd' + i
-
-      delete context.resetDisplayDevicesUrl.query[key]
-
-      var deviceUrl = url.parse(req.originalUrl, true)
-      var deviceQuery = deviceUrl.query
-      var availableDevice = {
-        device: device
-      }
-
-      if (deviceQuery[key] === '1') {
-        delete deviceQuery[key]
-        availableDevice.selected = true
-        displayDevices.push({
-          device: device
-        })
-      } else {
-        deviceQuery[key] = '1'
-      }
-
-      delete deviceUrl.search
-      availableDevice.url = url.format(deviceUrl)
-      availableDevices.push(availableDevice)
-    })
-
-    delete context.resetDisplayDevicesUrl.search
-    context.resetDisplayDevicesUrl = url.format(context.resetDisplayDevicesUrl)
-
-        // Example directory with JSON data files within?
-    var examplePath = project.findStyleguideFile(requestedPath)
-
-        // res.send(renderContent(config, context, '/main.hbs'));
-    if (require('./render-content')(config, req, res, context)) {
-      return
-    }
-
-        // Not a directory or a styleguide route? (ie: jquery.js)
-    if (!examplePath || !fs.statSync(examplePath).isDirectory()) {
-      next()
-      return
-    }
-
-        // Render the main template.
-    logger.success('URL: ' + req.originalUrl)
-    return res.send(renderPage(config, context, '/main.hbs'))
-  })
-
   app.use('/node-modules', (req, res, next) => {
     let absUrl = require.resolve(req.url.replace(/^\/+/g, ''))
     res.sendFile(absUrl)
@@ -174,9 +26,10 @@ module.exports = function (config) {
 
   app.use(express.static(config.build))
 
-    // Start the web server.
-  let server = app.listen(config.port, config.host, function () {
-    logger.success(`Started on http://${config.host}:${config.port}`)
+  const server = app.listen(config.port, config.host, () => {
+    const url = `http://${config.host}:${config.port}/styleguide.html`
+    logger.success(`Started on ${url}`)
+    opn(url)
   })
 
     // Handle server errors

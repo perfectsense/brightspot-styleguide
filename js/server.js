c830035ab4458e7afe66e1d304444635482320b1
Hyoo Lim <hyoolim@gmail.com>
2016-12-05 22:50:39 -0500

    Cleans up logging.

--- js/server.js
+++ js/server.js
@@ -3,37 +3,31 @@ const opn = require('opn')
 
 const logger = require('./logger')
 
-module.exports = function (config) {
+module.exports = config => {
   logger.welcome()
+
+  Object.keys(config).sort().forEach(key => {
+    logger.info(`Config: ${key}: ${JSON.stringify(config[key])}`)
+  })
 
   const app = express()
 
-    // Automatically generated placeholder images.
   app.use(require('./placeholder-image')())
-
-  Object.keys(config).sort().forEach(key => {
-    logger.success(`Config: ${key}: ${JSON.stringify(config[key])}`)
-  })
-
-  app.use('/node-modules', (req, res, next) => {
-    let absUrl = require.resolve(req.url.replace(/^\/+/g, ''))
-    res.sendFile(absUrl)
-  })
-
   app.use(express.static(config.build))
 
   const server = app.listen(config.port, config.host, () => {
     const url = `http://${config.host}:${config.port}/styleguide.html`
-    logger.success(`Started on ${url}`)
+
+    logger.success(`Server started on ${url}`)
     opn(url)
   })
 
-    // Handle server errors
-  server.on('error', function (err) {
-    if (err.errno === 'EADDRINUSE') {
-      logger.warn(`We tried starting the styleguide on ${config.host}:${config.port}, but it's already being used. Maybe you've got another styleguide running?`)
+  server.on('error', function (error) {
+    if (error.errno === 'EADDRINUSE') {
+      logger.error(`Another server already running on ${config.port}!`)
+
     } else {
-      console.log(err)
+      logger.error(`Unknown error! ${error}`)
     }
 
     this.close()

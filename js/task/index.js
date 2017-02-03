db1bdc9ad2e0ab11f10df331636077f3b1b58371
John Pencola <paulpencola@gmail.com>
2017-02-03 13:24:20 -0500

    Remove eslint disable comments and use fat-arrow syntax

--- js/task/index.js
+++ js/task/index.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-unused-vars */
-
-document.addEventListener('DOMContentLoaded', function (event) {
+document.addEventListener('DOMContentLoaded', (event) => {
   // DOM fully loaded and parsed
 }, false)
-
-/* eslint-enable no-unused-vars */

313f7cd3107a29d4b734d23b5bf3eca5d09b8ddc
John Pencola <paulpencola@gmail.com>
2017-04-05 17:00:59 -0400

    Improve by folding duplicate event listeners

--- js/comparison-mode.js
+++ js/comparison-mode.js
@@ -4,13 +4,12 @@ import Util from './util.js'
 export default {
   init: () => {
     $$(`.StyleguideExample-frame`)._.addEventListener(`load`, () => {
+      const examplePath = Util.locationSearchToObject(window.location.search).file
+
       $$(`.StyleguideComparison-frame`).forEach(el => {
         el.src = 'about:blank'
       })
-    })
 
-    $$(`.StyleguideExample-frame`)._.addEventListener(`load`, () => {
-      const examplePath = Util.locationSearchToObject(window.location.search).file
       $$(`.StyleguideComparison-group`).forEach(el => {
         el.removeAttribute(`data-visible`)
         if (el.dataset.examplePath && el.dataset.examplePath === examplePath) {

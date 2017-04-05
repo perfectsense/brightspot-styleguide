563f58f0aaed5678213b9817827ce5b8470bb698
John Pencola <paulpencola@gmail.com>
2017-04-05 16:27:43 -0400

    Unloads the comparison frame when an example frame initially loads

--- js/comparison-mode.js
+++ js/comparison-mode.js
@@ -4,6 +4,12 @@ import Util from './util.js'
 export default {
   init: () => {
     $$(`.StyleguideExample-frame`)._.addEventListener(`load`, () => {
+      $$(`.StyleguideComparison-frame`).forEach(el => {
+        el.src = 'about:blank'
+      })
+    })
+
+    $$(`.StyleguideExample-frame`)._.addEventListener(`load`, () => {
       const examplePath = Util.locationSearchToObject(window.location.search).file
       $$(`.StyleguideComparison-group`).forEach(el => {
         el.removeAttribute(`data-visible`)
